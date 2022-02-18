af407456fc34600bd72580ce6dd43fc655c6b83a
Cédryc Ruth <ceruth@epfc.eu>
2022-02-18 20:53:27 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\ShowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +19,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('artists', [ArtistController::class,'index'])->name('artist.index');
+Route::get('artists/{id}', [ArtistController::class,'show'])
+    ->where('id','[0-9]+')
+    ->name('artist.show');
+Route::get('artists/{id}/edit', [ArtistController::class,'edit'])
+    ->where('id','[0-9]+')
+    ->name('artist.edit');
+Route::put('artists/{id}', [ArtistController::class,'update'])
+    ->where('id','[0-9]+')
+    ->name('artist.update');
+
+Route::get('types', [TypeController::class,'index'])->name('type.index');
+Route::get('types/{id}', [TypeController::class,'show'])
+    ->where('id','[0-9]+')
+    ->name('type.show');
+
+Route::get('shows', [ShowController::class,'index'])->name('show.index');
+Route::get('shows/{id}', [ShowController::class,'show'])
+    ->where('id','[0-9]+')
+    ->name('show.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
