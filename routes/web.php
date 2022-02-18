8e5c32cc0fb981ba7e2e254b6a61e1098926924b
Cédryc Ruth <ceruth@domepfc.net>
2022-02-18 20:39:58 +0100

    Breeze install

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArtistController;
-use App\Http\Controllers\TypeController;
-use App\Http\Controllers\ShowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,23 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('artists', [ArtistController::class,'index'])->name('artist.index');
-Route::get('artists/{id}', [ArtistController::class,'show'])
-    ->where('id','[0-9]+')
-    ->name('artist.show');
-Route::get('artists/{id}/edit', [ArtistController::class,'edit'])
-    ->where('id','[0-9]+')
-    ->name('artist.edit');
-Route::put('artists/{id}', [ArtistController::class,'update'])
-    ->where('id','[0-9]+')
-    ->name('artist.update');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('types', [TypeController::class,'index'])->name('type.index');
-Route::get('types/{id}', [TypeController::class,'show'])
-    ->where('id','[0-9]+')
-    ->name('type.show');
-
-Route::get('shows', [ShowController::class,'index'])->name('show.index');
-Route::get('shows/{id}', [ShowController::class,'show'])
-    ->where('id','[0-9]+')
-    ->name('show.show');
+require __DIR__.'/auth.php';
